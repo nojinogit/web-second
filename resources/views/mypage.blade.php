def8746798399a0aca87a1e188fbaaa785a35ac4
nojino <nojinonoue@yahoo.co.jp>
2023-06-10 22:39:12 +0900

    test

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -8,20 +8,19 @@
 
 @section('content')
     <div class="container">
-        <div class="flex__item store-wrap">
+        <div class="store-wrap">
+            <h1>予約状況</h1>
             <div class="store-wrap__item" href="">
                 @foreach($reserves as $reserve)
-                <h1>{{$reserve->shop->name}}</h1>
-                <img src="" alt="" class="store-wrap__item-eyecatch">
-                <div class="store-wrap__item-content">
-                    <div>
-                        <p class="store-wrap__item-content-tag">#{{$reserve->date}}</p>
-                        <p class="store-wrap__item-content-tag">#{{$reserve->time}}</p>
+                    <div class="reserve-confirmation">
+                        <div class="reserve-confirmation-area">
+                            <img src="{{ asset('svg/時計.svg')}}" alt="" id="clock"><span id="reserve-num">予約{{$loop->iteration}}</span>
+                            <p><label for="">shop</label>&emsp;<span>{{$reserve->shop->name}}</span></p>
+                            <p><label for="">date</label>&emsp;<span id="output-date">{{$reserve->date}}</span></p>
+                            <p><label for="">time</label>&emsp;<span id="output-time">{{$reserve->time}}</span></p>
+                            <p><label for="">number</label>&emsp;<span id="output-number">{{$reserve->hc}}</span></p>
+                        </div>
                     </div>
-                    <div class="flex__item store-wrap">
-                        <p>※予約可能時間&emsp;11：00～22：00</p>
-                    </div>
-                </div>
                 @endforeach
             </div>
         </div>
